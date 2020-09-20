d1b4175b7563bb9aa0c184c875d9d61a0cb980d6
Albert Arrieta <albert.arrieta12@gmail.com>
2020-09-20 16:42:11 -0600

    Project setup, added bootstrap and currently creating construction page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ConstructionView from './views/ConstructionView';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div>
-      
+    <div className="text-center">
+      <ConstructionView /> 
     </div>
   );
 }
---
+++ src/views/ConstructionView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConstructionView(){
+    return(
+        <div>
+            <h1>Our website is currently undergoing construction</h1>
+            <img src='../images/logo.png' alt="logo"></img>
+        </div>
+    )
+}
+
+export default ConstructionView;
