192f6d24bc0dd3b82bf6890568cb9410998d7590
Albert Arrieta <albert.arrieta12@gmail.com>
2020-11-27 17:10:59 -0700

    footer and contact section revision

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faInbox } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 function ContactSection() {
   return (
       <div id="contact">
@@ -13,18 +14,50 @@ function ContactSection() {
           We are your team outside your office.
         </h4>
         </div>
-        <h5 className="text-orange mb-4 mt-4">
-              <FontAwesomeIcon icon={faPhone} size="md" className="contact-icon"/> 
-              (403)-703-9251
-        </h5>
 
-        <a href="mailto:aarrieta@albrondgsinc.com">
-        <button className="orange-btn btn shadow text-orange mt-4">
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-md-4">
+            <a class="text-orange" href="tel:14037039251">
+              <div className="contactCard hvr-sweep-to-bottom">
+                <FontAwesomeIcon icon={faPhone} size="3x" className="contact-icon"/> 
+                <h5 className="text-white mb-4 mt-4">+1 403-703-9251</h5>
+              </div>
+            </a>
+            </div>
+
+            <div className="col-md-4">
+
+              <a className="text-orange" href="mailto:aarrieta@albrondgsinc.com">
+              
+                <div className="contactCard hvr-sweep-to-bottom">
+                  <FontAwesomeIcon icon={faEnvelope} size="3x" className="contact-icon"/>
+                  <h5 className="text-white mb-4 mt-4">aarrieta@albrondgsinc.com</h5>
+                </div>
+
+              </a>
+            </div>
+
+            <div className="col-md-4">
+
+            <a class="text-orange" href="https://www.google.com/maps/place/Calgary,+AB/@51.0277555,-114.2279168,11z/data=!3m1!4b1!4m5!3m4!1s0x537170039f843fd5:0x266d3bb1b652b63a!8m2!3d51.0447331!4d-114.0718831">
+
+              <div className="contactCard hvr-sweep-to-bottom">
+                <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" className="contact-icon"/> 
+                <h5 className="text-white mb-4 mt-4">Calgary, Alberta</h5>
+              </div>
+
+            </a>
+            </div>
+          
         
-        <FontAwesomeIcon icon={faInbox} size="md" className="mr-2"/> 
-        EMAIL
-        </button>
-        </a>
+
+      
+        
+        
+          </div>
+        </div>
+        
 
 
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 function FooterSection() {
   return (
       <div id="footer">
-          <p className="mb-0 p-2">Albron Designs & Graphic Systems Incorporated <span className="text-orange">©2020 All rights reserved.</span> Website Designed and Developed by <a className="text-orange" href="https://alobyte.dev/">Albert Arrieta.</a></p>
+
+        <div className="container">
+          <div id="footerContact" className="container">
+            <div className="row">
+              <div className="col-4">
+                <a className="text-white" href="tel:14037039251">
+                <FontAwesomeIcon icon={faPhone} size="md" className="contact-icon"/> 
+                +1 403-703-9251
+                </a>
+              </div>
+              <div className="col-4">
+              <a className="text-white" href="mailto:aarrieta@albrondgsinc.com">
+              <FontAwesomeIcon icon={faEnvelope} size="md" className="contact-icon"/>
+              aarrieta@albrondgsinc.com
+              </a>
+              </div>
+              <div className="col-4">
+              <a className="text-white" href="https://www.google.com/maps/place/Calgary,+AB/@51.0277555,-114.2279168,11z/data=!3m1!4b1!4m5!3m4!1s0x537170039f843fd5:0x266d3bb1b652b63a!8m2!3d51.0447331!4d-114.0718831">
+                <FontAwesomeIcon icon={faMapMarkerAlt} size="md" className="contact-icon"/> 
+                Calgary, Alberta
+              </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <p className="mb-0 p-2">Albron Designs & Graphic Systems Incorporated <span className="text-orange">©2020 All rights reserved.</span></p>
+          <p>Website Designed and Developed by <a className="text-orange" href="https://alobyte.dev/">Albert Arrieta.</a></p>
+          
       </div>
   );
 }
