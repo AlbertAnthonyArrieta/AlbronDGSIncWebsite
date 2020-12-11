ae956959ed75758e43a378990d540f408e922b86
Albert Arrieta <albert.arrieta12@gmail.com>
2020-12-11 13:30:55 -0700

    Scroll spy offset and navbar reordering

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,12 +3,22 @@ import Logo from "../images/NAVLOGO.png";
 import { Link, animateScroll as scroll } from "react-scroll";
 function Navbar() {
   return (
-    <div id="navbar">
+    <div id="navbar" className="sticky-top">
       <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand">
-            <img src={Logo} height="70rem"></img>
-          </a>
+          <Link
+            to="landingSection"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-70}
+          >
+            <a className="navbar-brand">
+              <img src={Logo} height="70rem"></img>
+            </a>
+          </Link>
+
+          
           <button
             className="navbar-toggler"
             data-toggle="collapse"
@@ -19,38 +29,38 @@ function Navbar() {
           <div className="collapse navbar-collapse" id="navbarToggle">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-              <Link 
-                to="landingSection"
-                activeClass="active"
-                spy={true}
-                smooth={true}
-              >
-                <a className="nav-link">
-                  Home
-                </a>
-              </Link>
-              </li>
-              <li className="nav-item">
-              <Link 
-                to="serviceSection"
-                activeClass="active"
-                spy={true}
-                smooth={true}
-              >
-                <a className="nav-link">
-                  Our Services
-                </a>
-              </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="aboutSection"
+                <Link
+                  to="landingSection"
                   activeClass="active"
                   spy={true}
                   smooth={true}
+                  offset={-70}
                 >
-                  <a className="nav-link">
-                    About
-                  </a>
+                  <a className="nav-link">Home</a>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="aboutSection"
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                >
+                  <a className="nav-link">About</a>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="serviceSection"
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                >
+                  <a className="nav-link">Services</a>
                 </Link>
               </li>
               {/* <li className="nav-item">
@@ -59,15 +69,14 @@ function Navbar() {
                 </a>
               </li> */}
               <li className="nav-item">
-              <Link 
-                to="contact"
-                activeClass="active"
-                spy={true}
-                smooth={true}
-              >
-                <a className="nav-link">
-                  Contact
-                </a>
+                <Link
+                  to="contact"
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                >
+                  <a className="nav-link">Contact</a>
                 </Link>
               </li>
             </ul>
@@ -75,10 +84,7 @@ function Navbar() {
         </div>
       </nav>
     </div>
-    
   );
 }
-
-
 
 export default Navbar;
